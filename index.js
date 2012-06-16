0e4e878d86f2eb63ff7fe84071b63ce4145bf94e
Francis Gulotta <wizard@roborooter.com>
2012-06-15 22:36:04 -0400

    static fileserver

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 var express = require('express')
-  , mustache = require('mustache');
+  , mustache = require('./mustache')
+  , app = express.createServer()
+  , socket = require('socket.io')
+  , io = socket.listen(app);
 
-  
+var port = (process.argv[2] || 3000)
+  , viewPath = process.cwd() + '/views'
+  , staticPath = process.cwd() + '/public';
+
+
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.logger());
+  app.use(express.responseTime());
+  app.use(express.static(staticPath));
+});
+
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+
+
+app.listen(port);
+console.log("Listening on port: " + port);
